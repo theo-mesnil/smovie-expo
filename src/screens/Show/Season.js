83d9bdc496eebbc85d49d17f6ad4037b3a557db0
Theo Mesnil <mesniltheo@gmail.com>
2020-04-19 22:22:55 +0200

    fix(season): scrolling broken

--- src/screens/Show/Season.js
+++ src/screens/Show/Season.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import { format } from 'date-fns'
 import { useRoute } from '@react-navigation/native'
+import { ScrollView } from 'react-native'
 
 import { BasicLayout } from '../../layouts'
 import {
@@ -36,56 +37,58 @@ export function Season() {
           {seasonName}
         </TitleScreen>
       </Header>
-      <Centered marginTop={70} withMaxSize>
-        {seasonDetail ? (
-          <Box mt={40}>
-            {seasonDetail.episodes.map(episode => (
-              <S.Episode key={episode.name}>
-                {!!episode.still_path && (
-                  <Box mb="md" mr="sm" width="100%">
-                    <Thumb aspectRatio={16 / 9} backgroundUri={getImageUrl(episode.still_path)} />
-                  </Box>
-                )}
-                <Text fontSize="h2" mb="sm" numberOfLines={3} weight="bold">
-                  {`${episode.episode_number}. ${episode.name}`}
-                </Text>
-                {!!episode.overview && <Text color="dark400">{episode.overview}</Text>}
-                <Box
-                  alignItems="flex-end"
-                  flexDirection="row"
-                  justifyContent="space-between"
-                  mt="sm"
-                >
-                  {!!episode.vote_average && (
-                    <VoteAverage vote={episode.vote_average} weight="regular" />
+      <ScrollView showsVerticalScrollIndicator={false}>
+        <Centered marginTop={70} withMaxSize>
+          {seasonDetail ? (
+            <Box mt={40}>
+              {seasonDetail.episodes.map(episode => (
+                <S.Episode key={episode.name}>
+                  {!!episode.still_path && (
+                    <Box mb="md" mr="sm" width="100%">
+                      <Thumb aspectRatio={16 / 9} backgroundUri={getImageUrl(episode.still_path)} />
+                    </Box>
                   )}
-                  <Text>{format(new Date(episode.air_date), 'MM/dd/yyyy')}</Text>
+                  <Text fontSize="h2" mb="sm" numberOfLines={3} weight="bold">
+                    {`${episode.episode_number}. ${episode.name}`}
+                  </Text>
+                  {!!episode.overview && <Text color="dark400">{episode.overview}</Text>}
+                  <Box
+                    alignItems="flex-end"
+                    flexDirection="row"
+                    justifyContent="space-between"
+                    mt="sm"
+                  >
+                    {!!episode.vote_average && (
+                      <VoteAverage vote={episode.vote_average} weight="regular" />
+                    )}
+                    <Text>{format(new Date(episode.air_date), 'MM/dd/yyyy')}</Text>
+                  </Box>
+                </S.Episode>
+              ))}
+            </Box>
+          ) : (
+            // eslint-disable-next-line react/jsx-no-useless-fragment
+            <Box mt={40}>
+              {['1', '2', '3', '4', '5'].map(item => (
+                <Box
+                  backgroundColor="light600"
+                  borderRadius="md"
+                  key={item}
+                  mb="xl"
+                  paddingBottom="sm"
+                  paddingLeft="sm"
+                  paddingRight="sm"
+                  paddingTop="sm"
+                >
+                  <Box mr="sm" width="100%">
+                    <ThumbLoader aspectRatio={16 / 9} />
+                  </Box>
                 </Box>
-              </S.Episode>
-            ))}
-          </Box>
-        ) : (
-          // eslint-disable-next-line react/jsx-no-useless-fragment
-          <Box mt={40}>
-            {['1', '2', '3', '4', '5'].map(item => (
-              <Box
-                backgroundColor="light600"
-                borderRadius="md"
-                key={item}
-                mb="xl"
-                paddingBottom="sm"
-                paddingLeft="sm"
-                paddingRight="sm"
-                paddingTop="sm"
-              >
-                <Box mr="sm" width="100%">
-                  <ThumbLoader aspectRatio={16 / 9} />
-                </Box>
-              </Box>
-            ))}
-          </Box>
-        )}
-      </Centered>
+              ))}
+            </Box>
+          )}
+        </Centered>
+      </ScrollView>
     </BasicLayout>
   )
 }
